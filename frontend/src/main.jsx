b008bba1e63d81e2f1c0f7a67a797f7753482341
amaliahm <a.ghandouz@esi-sba.dz>
2025-07-06 19:32:48 +0100

    update app file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
+import { GlobalProvider } from './context/globalContext'
+import { GlobalStyle } from './styles/globalStyle'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GlobalStyle />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
   </StrictMode>,
 )
